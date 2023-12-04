7cb8dab683df2c2b8ea557c1d7d3fd08c4eb2580
Joaquin Orozco <joaquincuervito8@gmail.com>
2023-12-04 14:45:39 -0300

    ultimo commit (pagina finalizada)

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const listaPokemon =document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 const botonBuscar = document.querySelectorAll(".buscar");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
+let arrPok = [];
 
   for (let i = 1; i<= 151; i++){
     fetch(URL + i)
         .then((response) => response.json())
-        .then(data =>  {mostarPokemon(data);listaPokemon.data = data;})
+        .then(data =>  {
+            mostarPokemon(data);
+            arrPok.push(data);
+        })
   }
+  listaPokemon.data = arrPok;
 
   function mostarPokemon(poke){
     let tipos = poke.types.map((type) =>  
@@ -50,14 +55,61 @@ let URL = "https://pokeapi.co/api/v2/pokemon/";
 
   }
 
-  function buscar() {
-    let tarjetas = document.querySelector("#todos");
-    var data = document.querySelector("#listaPokemon").data;
-    console.dir(data);
-    var busqueda = document.querySelector("#listaPokemon").value - 1;
-    let URL = data.results[busqueda].URL;
+  function mostarPokemon1(poke){
+    let tipos = poke.types.map((type) =>  
+        `<p class="${type.type.name} tipo">${type.type.name}</p>`
+    );
+    tipos = tipos.join('');
 
-console.dir(data);
+    let pokeId = poke.id.toString();
+    if (pokeId.length === 1){
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2){
+        pokeId = "0" + pokeId;
+    }
+
+
+     const div = document.createElement("div");
+     div.classList.add("pokemon");
+     div.innerHTML = `
+     <p class="pokemon-id-back">#${pokeId}</p>
+     <div class="pokemon-imagen">
+         <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+     </div>
+     <div class="pokemon-info">
+         <div class="nombre-contenedor">
+             <p class="pokemon-id">#${pokeId}</p>
+             <h2 class="pokemon-nombre">${poke.name}</h2>
+             <div class="pokemon-tipos">
+                 ${tipos}
+             </div>
+             <div class="pokemon-stats">
+                 <p class="stat">${poke.height}m</p>
+                 <p class="stat">${poke.weight}kg</p>
+             </div>
+         </div>
+     </div>
+ </div>`;
+listaPokemon.innerHTML = "";
+ listaPokemon.append(div);
+
+  }
+
+
+
+  function buscar() {
+    let data = document.querySelector("#listaPokemon").data;
+    
+    let busqueda = document.querySelector("#nPokemon").value;
+
+    data.forEach((element)=>{
+if(element.id == busqueda){
+    console.log(element);
+    mostarPokemon1(element);
+}
+    });
+  }
+/*
     if(busqueda>=0){
       fetch(URL)
             .then((response) => response.json())
@@ -68,6 +120,7 @@ console.dir(data);
             })
     }
   }
+*/
 
 botonesHeader.forEach(boton=> boton.addEventListener("click", (event) =>{
     const botonId = event.currentTarget.id;
